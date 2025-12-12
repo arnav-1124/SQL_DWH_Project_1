6a7b8b29da5b02b169b5277771d0aec900e926a7
arnav-1124 <arnavgupta112@gmail.com>
2025-12-13 00:02:25 +0530

    Create and replace tables in bronze schema

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -1,23 +1,78 @@
-/*
--------------------------------------
-CREATING DATABASE AND SCHEMAS
--------------------------------------
 
-Script Purpose: Create a new database name 'DataWarehouse' and within that creating
-				3 schemas ('bronze', 'silver' and 'gold').
+-- Creating OR Replacing Table for 'cust_info.csv'
+IF OBJECT_ID ('bronze.crm_cust_info', 'U') IS NOT NULL
+	DROP TABLE bronze.crm_cust_info;
 
-*/
+CREATE TABLE bronze.crm_cust_info (
+	cst_id INT,
+	cst_key NVARCHAR(50),
+	cst_firstname  NVARCHAR(50),
+	cst_lastname NVARCHAR(50),
+	cst_marital_status NVARCHAR(50),
+	cst_gndr NVARCHAR(50),
+	cst_create_date DATE
+);
 
-USE master;
-GO
+-- Creating OR Replacing Table for 'prd_info.csv'
+IF OBJECT_ID ('bronze.crm_prd_info', 'U') IS NOT NULL
+	DROP TABLE bronze.crm_prd_info;
 
--- Creating a new Database named "DataWarehouse"
-CREATE DATABASE DataWarehouse;
+CREATE TABLE bronze.crm_prd_info (
+	prd_id INT,
+	prd_key NVARCHAR(50),
+	prd_nm NVARCHAR(50),
+	prd_cost INT,
+	prd_line NVARCHAR(50),
+	prd_start_dt DATE,
+	prd_end_dt DATE
+);
 
--- Creating all 3 Schemas:
-CREATE SCHEMA bronze; 
-GO
-CREATE SCHEMA silver;
-GO
-CREATE SCHEMA gold;
+-- Creating OR Replacing Table for 'sales_details.csv'
+IF OBJECT_ID ('bronze.crm_sales_details', 'U') IS NOT NULL
+	DROP TABLE bronze.crm_sales_details;
+
+CREATE TABLE bronze.crm_sales_details(
+sls_ord_num NVARCHAR(50),
+sls_prd_key NVARCHAR(50), 
+sls_cust_id INT, 
+sls_order_dt INT, 
+sls_ship_dt INT, 
+sls_due_dt INT, 
+sls_sales INT, 
+sls_quantity INT, 
+sls_price INT 
+);
+
+-- Creating OR Replacing Table for 'cust_az12.csv'
+IF OBJECT_ID ('bronze.erp_cust_az12', 'U') IS NOT NULL
+	DROP TABLE bronze.erp_cust_az12;
+
+CREATE TABLE bronze.erp_cust_az12 (
+cid NVARCHAR(50),
+bdate DATE,
+gen NVARCHAR(50)
+);
+
+-- Creating OR Replacing Table for 'loc_a101.csv'
+IF OBJECT_ID ('bronze.erp_loc_a101', 'U') IS NOT NULL
+	DROP TABLE bronze.erp_loc_a101;
+
+CREATE TABLE bronze.erp_loc_a101 (
+cid NVARCHAR(50),
+cntry NVARCHAR(50)
+);
+
+
+-- Creating OR Replacing Table for 'px_cat_g1v2.csv'
+IF OBJECT_ID ('bronze.erp_px_cat_g1v2', 'U') IS NOT NULL
+	DROP TABLE bronze.erp_px_cat_g1v2;
+
+CREATE TABLE bronze.erp_px_cat_g1v2 (
+id NVARCHAR(50),
+cat NVARCHAR(50),
+subcat NVARCHAR(50),
+maintenance NVARCHAR(50)
+);
+
+
 
